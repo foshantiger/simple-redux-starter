3eda15385f2478bdd933bf10998c62bb9b9f70c8
杨名远 <76472611@qq.com>
2016-06-06 18:41:27 +0800

    add all

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Redux Production
+
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+
+export class Hello extends React.Component {
+    static propTypes = {
+
+    };
+
+    state = {
+
+    };
+
+    render () {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Hello;
+
